65c0b210fb2ae4e7935034af879a2953a10bfea1
andreweinstein0206-oss <andreweinstein0206@gmail.com>
2026-09-16 05:36:40 +0800

    eto na

---
+++ ATM.cpp
@@ -0,0 +1,27 @@
+
+//ATM SIMULATION DATA STRUCTURE AND ALGORITHM PROJECT #1 
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <limits>
+#include <fstream>
+
+#define MAX 6
+#define MAX_USERS 100
+using namespace std;
+
+struct Userinfo
+{
+    string name; 
+    int pin, age, balance, withdraw, deposit, bday, contact, Ideposit;
+
+};
+
+class useraccount 
+{
+   private:
+   Userinfo user[MAX_USERS];
+   int userCount;
+   int locateUser(string name, int pin);
+   int balancecheck(int );
